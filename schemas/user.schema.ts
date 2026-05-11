ccdc6e178ae9cc7d64517bd4830a00f4abe182cc
amir abaris <thisisamirabaris@gmail.com>
2026-05-11 10:45:48 +0330

    fix: schema rename

--- schemas/user.schema.ts
+++ schemas/user.schema.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod'
-import { createNoteSchema, noteParamsSchema, updateNoteSchema } from './note.schema'
+
 export const createUserSchema = z.object({
   name: z.string().min(3, 'Name is required').max(255, 'Name too long'),
   username: z.string().min(3, 'Username is required').max(255, 'Username too long'),
-  
 })
 
 export const updateUserSchema = createUserSchema.partial()
@@ -12,15 +11,15 @@ export const userParamsSchema = z.object({
   id: z.uuid('Invalid user   ID format'),
 })
 
-export const noteQuerySchema = z.object({
+export const userQuerySchema = z.object({
   page: z.coerce.number().int().positive().default(1),
   limit: z.coerce.number().int().min(1).max(100).default(10),
   search: z.string().optional(),
 })
 
 export default {
-  create: createNoteSchema,
-  update: updateNoteSchema,
-  params: noteParamsSchema,
-  query: noteQuerySchema,
+  create: createUserSchema,
+  update: updateUserSchema,
+  params: userParamsSchema,
+  query: userQuerySchema,
 }
